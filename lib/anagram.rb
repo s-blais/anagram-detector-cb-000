48ff9cc93ed15f506da66d0ae79dec46a170b32d
Stephen Blais <stephen@s-blais.com>
2019-08-29 18:55:50 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize (word)
+    @word = word
+
+  end
+
+  def match (possible_anagrams)
+    possible_anagrams.select do |w|
+      w.split("").sort == word.split("").sort
+    end
+    #compares sorted contents of word variable to sorted contents of each word and returns array of the successful anagrams
+  end
+
+
+end
