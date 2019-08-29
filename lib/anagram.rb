a2425c8a05aaf9534c1d5be0756ba301b800d229
Stephen Blais <stephen@s-blais.com>
2019-08-29 18:59:34 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,15 +4,12 @@ class Anagram
 
   def initialize (word)
     @word = word
-
   end
 
   def match (possible_anagrams)
     possible_anagrams.select do |w|
       w.split("").sort == word.split("").sort
     end
-    #compares sorted contents of word variable to sorted contents of each word and returns array of the successful anagrams
   end
-
 
 end
